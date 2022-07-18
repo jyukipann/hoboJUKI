f488a465b870ac3344c86ed72bddbc73e77dca57
jyukipann <jyukipann@yahoo.co.jp>
2022-07-18 18:15:10 +0900

    T5Tokenizer

--- train/test_train.py
+++ train/test_train.py
@@ -50,7 +50,7 @@ from transformers.testing_utils import CaptureLogger
 from transformers.trainer_utils import get_last_checkpoint
 from transformers.utils import check_min_version, send_example_telemetry
 from transformers.utils.versions import require_version
-
+from transformers import T5Tokenizer
 
 # Will error if the minimal version of Transformers is not installed. Remove at your own risks.
 check_min_version("4.21.0.dev0")

65b584535b8379915d526d6cd0121c1b70be0889
JUKI <jyukipann@yahoo.co.jp>
2022-08-04 21:21:05 +0900

    generator

--- playground/hoboJUKI_generator.py
+++ playground/hoboJUKI_generator.py
@@ -5,8 +5,10 @@ from transformers import T5Tokenizer, AutoModelForCausalLM
 
 print("load models")
 tokenizer = T5Tokenizer.from_pretrained("rinna/japanese-gpt-1b")
-model = AutoModelForCausalLM.from_pretrained("output/")
-print("torch.cuda.is_available()",torch.cuda.is_available())
+tokenizer.add_tokens(["[TWT]","[REP]", "[UNK]"])
+model = AutoModelForCausalLM.from_pretrained("output_v1/")
+model.resize_token_embeddings(len(tokenizer))
+# print("torch.cuda.is_available()",torch.cuda.is_available())
 
 if torch.cuda.is_available():
     model = model.to("cuda")
@@ -21,12 +23,14 @@ while True:
             max_length=100,
             min_length=100,
             do_sample=True,
-            top_k=500,
+            top_k=50,
             top_p=0.95,
             pad_token_id=tokenizer.pad_token_id,
             bos_token_id=tokenizer.bos_token_id,
             eos_token_id=tokenizer.eos_token_id,
-            # bad_word_ids=[[tokenizer.unk_token_id]]
+            bad_word_ids=[[tokenizer.unk_token_id]],
+            num_beams=5, 
+            early_stopping=True,
         )
     output = tokenizer.decode(output_ids.tolist()[0])
     print(output)
--- playground/how_to_add_tokens.py
+++ playground/how_to_add_tokens.py
@@ -1,6 +1,6 @@
 # https://github.com/huggingface/transformers/issues/6789
 
-from transformers import T5Tokenizer
-tokenizer = T5Tokenizer.from_pretrained("t5-base")
-tokenizer.add_tokens(['<some_token_1>', '<some_token_2'>])
-# model.resize_token_embeddings(len(tokenizer))
+# from transformers import T5Tokenizer
+# tokenizer = T5Tokenizer.from_pretrained("t5-base")
+# tokenizer.add_tokens(['<some_token_1>', '<some_token_2>'])
+# # model.resize_token_embeddings(len(tokenizer))
